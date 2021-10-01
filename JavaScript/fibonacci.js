405b9bd7cf1ccda17ebd0b5f17caee0cc114887b
Vaibhav Dadhich <drvaibhavdadhich@gmail.com>
2021-10-01 13:35:44 +0530

    Fixes

--- JavaScript/fibonacci.js
+++ JavaScript/fibonacci.js
@@ -1,3 +1,15 @@
+
+/* 
+------------------------------------------------------------------------------------------------
+USERNAME: VAIBHAV1405
+DESCRIPTION: THIS PROGRAM GENERATES FINONACCI SERIES
+DATE: 1/10/2021
+------------------------------------------------------------------------------------------------
+*/
+
+
+
+
 function fibonacciSequence(number) {
     for (var fibonacci = [0, 1], i = 1; i < number; i++) // Starts array at index [0, 1] (1).
         fibonacci.push(fibonacci[i] + fibonacci[i - 1]) // Pushes current index + previous index (current index -1).
