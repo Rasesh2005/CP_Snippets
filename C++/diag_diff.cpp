1009b4f1d0f12a730e2929f2caaa2f51bdd9a9e0
Sundaram Srivastava <72743942+Deadeye001@users.noreply.github.com>
2021-10-01 15:46:30 +0530

    Create diag_diff.cpp

---
+++ C++/diag_diff.cpp
@@ -0,0 +1,33 @@
+/* 
+------------------------------------------------------------------------------------------------
+USERNAME: Deadeye001
+DESCRIPTION: THIS CODE FINDS THE ABSOLUTE DIFFERENCE BETWEEN THE DIAGONALS OF A SQUARE MATRIX OF SIZE NxN
+DATE: 1/10/2021
+------------------------------------------------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //APPLICATION
+    int n;
+    cin>>n;
+  
+    //SNIPPET    
+    int arr[n][n];
+    int lr=0,rl=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+            if((i+j+2)==(n+1))
+            rl+=arr[i][j];
+            if(i==j)
+            lr+=arr[i][j];
+        }
+    }    
+    cout<<abs(lr-rl); 
+    return 0;
+}
