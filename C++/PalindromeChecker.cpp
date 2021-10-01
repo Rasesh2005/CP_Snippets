b1461fce72e39050110ec65073e533a4ab1e2cfa
abhilashkpy <64542750+abhilashkpy@users.noreply.github.com>
2021-10-01 18:45:25 +0530

    Update PalindromeChecker.cpp
    Changes made:  the main code has been made as a function and it is being then called from inside the main() function.

--- C++/PalindromeChecker.cpp
+++ C++/PalindromeChecker.cpp
@@ -10,25 +10,32 @@ DATE: 1/10/2021
 #include <iostream>
 using namespace std;
 
+int checkPal(int number)
+{
+  int x, rem, reverse=0;
+  x = number;
+   
+  while( number!=0 )
+  {
+     rem = number % 10;
+     reverse = reverse*10 + rem;
+     number /= 10;
+  }
+
+  cout << " The reverse of the number is: " << reverse << endl;
+  if ( reverse == x ) return 0;
+  else return 1;
+}
+
+
 int main()
 {
-   int x, number, digit, reverse = 0;
+   int number;
 
   cout << "ENTER A NUMBER: ";
-  cin >> numbe;
-
-  x = number;
-
-  do
-  {
-     digit = number % 10;
-     reverse = (reverse * 10) + digit;
-     number = number / 10;
- } while (number != 0);
-  
-  
-cout << " The reverse of the number is: " << reverse << endl;
-  if (x == reverse)
+  cin >> number;
+   
+  if(checkPal(number) == 0)
       cout << " THE NUMBER IS A PALINDROME";
   else
       cout << " THE NUMBER IS NOT A PALINDROME.";
