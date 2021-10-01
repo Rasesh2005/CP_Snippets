865f09b96e70a549b2018354bbff9ac34e115621
Sundaram Srivastava <72743942+Deadeye001@users.noreply.github.com>
2021-10-01 17:04:55 +0530

    Update and rename diag_diff.cpp to Diagonal_Difference.cpp

--- C++/Diagonal_Difference.cpp
+++ C++/Diagonal_Difference.cpp
@@ -3,6 +3,20 @@
 USERNAME: Deadeye001
 DESCRIPTION: THIS CODE FINDS THE ABSOLUTE DIFFERENCE BETWEEN THE DIAGONALS OF A SQUARE MATRIX OF SIZE NxN
 DATE: 1/10/2021
+
+SAMPLE INPUT: 
+3 
+11 2 4 
+4 5 6 
+10 8 -12
+
+SAMPLE OUTPUT: 
+15
+
+EXPLANATION: 
+The sum of the elements of the left diagonal = 11+5+(-12) =4 
+The sum of the elements of the right diagonal = 10+5+4 =19 
+Hence, the absolute difference= |4-19| = 15.
 ------------------------------------------------------------------------------------------------
 */
 
