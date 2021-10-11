9c05f2100559ce6f93a4e9d29a2a4489280a7050
kartikpmadhu <91833809+kartikpmadhu@users.noreply.github.com>
2021-10-11 23:53:48 +0530

    Create MatrixDeterminant.c
    Program in C to calculate the determinant of a matrix

---
+++ C/MatrixDeterminant.c
@@ -0,0 +1,34 @@
+/* 
+------------------------------------------------------------------------------------------------
+USERNAME: KARTIKPMADHU
+DESCRIPTION: THIS PROGRAM CALCULATES THE DETERMINANT OF A GIVEN MATRIX
+DATE: 11/10/2021
+------------------------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#define MATRIX_SIZE 2 
+
+int main()
+{
+    int A[MATRIX_SIZE][MATRIX_SIZE];
+    int row, column;
+    long determinant;
+
+    /* Input elements in matrix A from user */
+    printf("Enter elements in matrix of size 2x2: \n");
+    for(row=0; row<MATRIX_SIZE; row++)
+    {
+        for(column=0; column<MATRIX_SIZE; column++)
+        {
+            scanf("%d", &A[row][column]);
+        }
+    }
+
+
+    determinant = (A[0][0] * A[1][1]) - (A[0][1] * A[1][0]);
+
+    printf("Determinant of matrix A = %ld", determinant);
+
+    return 0;
+}
