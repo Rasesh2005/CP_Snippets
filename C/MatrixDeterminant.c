a621457a12857aa32a53afc6e582cfad25ccc271
kartikpmadhu <91833809+kartikpmadhu@users.noreply.github.com>
2021-10-12 11:31:26 +0530

    Update MatrixDeterminant.c
    Updated the program by making it in wrapping format.

--- C/MatrixDeterminant.c
+++ C/MatrixDeterminant.c
@@ -6,29 +6,49 @@ DATE: 11/10/2021
 ------------------------------------------------------------------------------------------------
 */
 
-#include <stdio.h>
-#define MATRIX_SIZE 2 
+#include<stdio.h>
+#include<math.h>
 
-int main()
-{
-    int A[MATRIX_SIZE][MATRIX_SIZE];
-    int row, column;
-    long determinant;
+int determinant( int matrix[10][10], int n) {
+int det = 0;
+int submatrix[10][10];
+if (n == 2)
+ return ((matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]));
+else {
+   for (int x = 0; x < n; x++) {
+     int subi = 0;
+     for (int i = 1; i < n; i++) {
+       int subj = 0;
+       for (int j = 0; j < n; j++) {
+          if (j == x)
+           continue;
+           submatrix[subi][subj] = matrix[i][j];
+           subj++;
+            }
+            subi++;
+         }
+         det = det + (pow(-1, x) * matrix[0][x] * determinant( submatrix, n - 1 ));
+      }
+   }
+   return det;
+}
 
-    /* Input elements in matrix A from user */
-    printf("Enter elements in matrix of size 2x2: \n");
-    for(row=0; row<MATRIX_SIZE; row++)
-    {
-        for(column=0; column<MATRIX_SIZE; column++)
-        {
-            scanf("%d", &A[row][column]);
-        }
-    }
 
-
-    determinant = (A[0][0] * A[1][1]) - (A[0][1] * A[1][0]);
-
-    printf("Determinant of matrix A = %ld", determinant);
-
-    return 0;
+int main() {
+   int n, i, j;
+   int matrix[10][10];
+   printf("Enter the size of matrix:\n";
+   scanf("%d",&n);
+   printf("Enter the elements of matrix:\n";
+   for (i = 0; i < n; i++)
+   for (j = 0; j < n; j++)
+   scanf("%d", &matrix[i][j])
+   printf("The entered matrix is: \t");
+   for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++)
+      printf("%d   ", matrix[i][j]);
+      printf("\n");
+   }
+   printf("Determinant of the matrix is %d ", determinant(matrix, n));
+   return 0;
 }
